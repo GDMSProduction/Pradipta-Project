9fa47937e49fd9182fba55e40c5c7e47fe37f5a2
Syawadhilah Pradipta <dipta0988@gmail.com>
2019-07-25 19:27:13 -0400

    Completed JSON Parse

---
+++ app/src/main/java/com/example/pictureuploader/ImageObject.java
@@ -0,0 +1,22 @@
+public class ImageObject {
+    String id;
+    String url;
+    String imageurl;
+    String PhotographerName;
+
+
+    public ImageObject(String id, String url, String imageurl, String photographerName){
+        this.id = id;
+        this.url = url;
+        this.imageurl = imageurl;
+        this.PhotographerName = photographerName;
+
+    }
+
+//    @androidx.annotation.NonNull
+    @Override
+    public String toString() {
+        return "id: " + id + "\nurl: " + url + "\nimage url: " + imageurl + "\nPhotographer Name: " + PhotographerName;
+
+    }
+}
